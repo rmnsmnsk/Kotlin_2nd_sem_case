dfd942decd3cf721530cfaba8c6d5a13aa72c51f
Roman_Semensky <136500209+rmnsmnsk@users.noreply.github.com>
2026-04-10 10:17:24 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,7 @@ dependencies {
     // JUnit 5
     testImplementation(platform("org.junit:junit-bom:5.10.2"))
     testImplementation("org.junit.jupiter:junit-jupiter")
-    testRuntimeOnly("org.junit.Oplatform:junit-platform-launcher")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 tasks.test {
